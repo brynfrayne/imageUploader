5b4dcca722eef8975fb0c111c7c5875c62cc4e17
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-03-11 11:01:09 -0800

    adding first commit to git repo

---
+++ src/components/Button/Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+// Style the Button component
+const Button = styled.button`
+  /* Insert your favorite CSS code to style a button */
+    background-color: #2F80ED;
+    border: none;
+    border-radius: .5rem;
+    color: white;
+    padding: .5rem 1rem;
+    cursor:pointer;
+`
+export default function FileUploader(props) {
+  // Create a reference to the hidden file input element
+  const hiddenFileInput = React.useRef(null);
+  
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file 
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    props.handleFile(fileUploaded);
+  };
+  return (
+    <>
+      <Button onClick={handleClick} >
+        Upload a file
+      </Button>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        style={{display: 'none'}} 
+      />
+    </>
+  );
+
+  }
---
+++ src/components/SuccessCard/SuccessCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Stock from '../../assets/image-uploader-master/stock.jpg';
+import checkIcon from '../../assets/image-uploader-master/greenCheckIcon.jpeg';
+import FileUploader from '../Button/Button';
+import '../UploadCard/UploadCard.css';
+import './SuccessCard.css';
+
+export default function SuccessCard() {
+  return (
+    <div className='uploadCard'>
+        <h1 className='uploadCard__title'>
+            Uploaded Succesfully
+        </h1>
+        <img src={checkIcon} alt="" className='checkIcon'/>
+        <div>
+            <img src={Stock} alt="" className='uploaded-image'/>
+        </div>
+        <div className='url__box'>
+            <p className='url__text'>wwww.madeupURL.org</p>
+            <FileUploader />
+        </div>
+    </div>
+  )
+}
